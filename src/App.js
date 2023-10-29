4e87d893431e6b6d91efb459198f52f6c0fd051c
deepika <singla.deepika28@gmail.com>
2023-10-29 12:50:26 -0700

    third commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ToDo from './content/ToDo';
+// import Module1 from './content/Module1';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDo />
+      {/* <Module1 /> */}
+    </div>
+  );
+}
+
+export default App;
